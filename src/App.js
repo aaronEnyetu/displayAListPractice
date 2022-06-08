d5f3556ec083dd9d82de82f1a1e055330e27034c
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-08 14:17:32 -0700

    Simple list of objects complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 // import your arrays here
 import { fruits } from './fruits.js';
 import FruitsList from './FruitList';
+
+import { plants } from './plants.js';
+import PlantsList from './PlantsList';
+
 console.log(fruits);
 
 function App() {
   return (
     <div className="App">
       <FruitsList fruits={fruits} />
+      <hr/>
+      <PlantsList plants={plants}/>
     </div>
   );
 }
