ba922db66609aad58e3b127692968a30dca5eed4
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-09 12:57:44 -0700

    made some changes in the package

--- src/FruitList.js
+++ src/FruitList.js
@@ -2,7 +2,7 @@ import FruitItem from './FruitItem';
 import React from 'react';
 
 export default function FruitsList({ fruits }) { 
-  console.log(fruits);
+
   return <div>
     {
       fruits.map((fruit, i) => <FruitItem fruit={fruit} key={fruit + i} />)
